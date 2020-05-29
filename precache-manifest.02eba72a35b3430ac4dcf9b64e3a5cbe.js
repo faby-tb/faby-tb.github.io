fdb17f6318ffe7de7ac74f811e99c3b0bf9d243e
Fabián Torres <54631281+faby-tb@users.noreply.github.com>
2020-05-29 07:34:42 -0600

    U#8

---
+++ precache-manifest.02eba72a35b3430ac4dcf9b64e3a5cbe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368229e1b3c175dc6bce59cf3eeff3f6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6b422ca9ad1632ed295f",
+    "url": "./static/css/main.a3baf147.chunk.css"
+  },
+  {
+    "revision": "032d0c3e92b3e6d88dda",
+    "url": "./static/js/2.726ebbb4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.726ebbb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b422ca9ad1632ed295f",
+    "url": "./static/js/main.ce7bb639.chunk.js"
+  },
+  {
+    "revision": "dcce7fed1363c375c9c4",
+    "url": "./static/js/runtime-main.310ffc9b.js"
+  },
+  {
+    "revision": "aff5432ba3a63f3589a58f6af1ca4756",
+    "url": "./static/media/The Weeknd - False Alarm (Audio).aff5432b.mp3"
+  },
+  {
+    "revision": "439750f9e799e49dd09b9073a181d8b8",
+    "url": "./static/media/placeholder_600x400.439750f9.svg"
+  },
+  {
+    "revision": "7038a84588c88abd40093dcbf7a4d1b5",
+    "url": "./static/media/the-weeknd-blinding-lights.7038a845.jpg"
+  },
+  {
+    "revision": "a7ed7802885b78ea68f3839717637af1",
+    "url": "./static/media/the-weeknd.a7ed7802.jpg"
+  }
+]);
